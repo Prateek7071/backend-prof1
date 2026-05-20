b6edb5bf480d7a3eb18b0facb963228ce8c6688e
Prateek7071 <tiwariprateek123@gmail.com>
2026-05-21 00:38:03 +0530

    added password encription before saving to db

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from JsonWebToken
+import bcrypt from "bcryptjs";
 
 const userSchema = new Schema({
   username: {
@@ -43,5 +45,21 @@ const userSchema = new Schema({
     type: String
   }
 }, { timestamps: true })
+
+// userSchema.pre(()=>{}) // dont use arrow function here as it does not have reference of this. it doesnt know the context and context here is very important  
+
+// and async cause it takes time
+// 
+// userSchema(async function (next) {
+//   this.password = bcrypt.hash(this.password, 10) //here 10 is the number of rounds
+//   next()
+// }) at this state it will always run ex, if say user changed avatar it will update the password to so we want it to only do that when we send pasword field
+
+// pre is used to do something just before the data is saved to db
+userSchema(async function (next) {
+  if(!this.isModified("password")) return next() //need to pass as string, also this is a negative text if not motified move on.
+  this.password = bcrypt.hash(this.password, 10) //here 10 is the number of rounds
+  next()
+})
 
 export const User = mongoose.model("User", userSchema)
