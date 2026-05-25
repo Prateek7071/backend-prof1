355c177f43f6cd3e67dba94f20c73e9a34ab8f6e
Prateek7071 <tiwariprateek123@gmail.com>
2026-05-25 20:31:53 +0530

    added login and logout routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
-
+import verifyJWT from "../middlewares/auth.middleware.js"
 const router = Router()
 
 router.route("/register").post(
@@ -17,5 +17,10 @@ router.route("/register").post(
   ]),
   registerUser
 )
+
+router.route("/login").post(loginUser)
+
+//secure route
+router.route("/logout").post(verifyJWT,logoutUser) //here verifyJWT is a middleware and contains next so when its done goes to logoutUser // and now we logoutUser has access to user back in user.controller
 
 export default router
