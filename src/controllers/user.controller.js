351b5d4ba224f73683e86f09de24bcd6da040c9c
Prateek7071 <tiwariprateek123@gmail.com>
2026-05-26 01:15:56 +0530

    added method to getCurrentUser

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -268,6 +268,10 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
   }
 })
 
-
+const getCurrentUser = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(200, req.user, "Current user fetched successfully")
+})
 
 export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword }
