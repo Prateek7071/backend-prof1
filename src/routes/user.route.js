bc23d913c1d3b8f5ba74400e09063f17eb10710a
Prateek7071 <tiwariprateek123@gmail.com>
2026-05-29 02:08:01 +0530

    updated routes for verified user actions

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,20 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCover
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js"
+import multer from "multer";
 const router = Router()
 
 router.route("/register").post(
@@ -23,5 +36,12 @@ router.route("/login").post(loginUser)
 //secure route
 router.route("/logout").post(verifyJWT, logoutUser) //here verifyJWT is a middleware and contains next so when its done goes to logoutUser // and now we logoutUser has access to user back in user.controller
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/update-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)//will only update necessary ones, if we do post will update everything which we dont want here.
+router.route("update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("update-coverImage").patch(verifyJWT, upload.single("coverImage"), updateUserCover)
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile) //here cause in the getUserChannelProfile we using req.params we need to focus on route that it contains username like this to use it later.
+router.route("/watch-history").get(verifyJWT, getWatchHistory)//user is not sending anything so we just use get
 
 export default router
