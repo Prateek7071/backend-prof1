6fa298eac4ed958c258e47c4f1e9e400cf9ea4cc
Prateek7071 <tiwariprateek123@gmail.com>
2026-05-31 22:28:59 +0530

    updated

--- src/models/subscription.models.js
+++ src/models/subscription.models.js
@@ -1,15 +1,19 @@
 import mongoose, { Schema } from "mongoose";
 
-const subscriptionSchema = Schema({
+const subscriptionSchema =new Schema({
   subscriber: {
     type: Schema.Types.ObjectId, //one who's subscribing 
-    ref: "User"
+    ref: "User",
+    required: true
   },
   channel: {
     type: Schema.Types.ObjectId, //one who subs are subscribing to
-    ref: "User"
+    ref: "User",
+    required: true
   }
   
 }, { timestamps: true })
+
+subscriptionSchema.index({ subscriber: 1, channel: 1 }, { unique: true })
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema)
