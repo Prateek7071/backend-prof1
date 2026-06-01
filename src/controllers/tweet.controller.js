3509c5b33ec9a7e55726727ade9e0e819a704ab6
Prateek7071 <tiwariprateek123@gmail.com>
2026-06-01 18:30:12 +0530

    bug fixed

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -41,16 +41,30 @@ const getUserTweets = asyncHandler(async (req, res) => {
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
+  const { tweetId } = req.params
+  if (!tweetId) {
+    throw new ApiError(400, "Tweet doesnt exist")
+  }
   const { content } = req.body
   if (!content) {
     throw new ApiError(400, "Content required")
   }
 
-  const updatedTweet = await Tweet.findByIdAndUpdate(req.user?._id, {
-    $set: {
-      content
-    }
+  const isAuthorised = await Tweet.findOne({
+    owner: req.user?._id,
+    _id: tweetId
   })
+  let updateTweet = "";
+  if (!isAuthorised) {
+    throw new ApiError(403,"Unauthorised to update tweet")
+  }
+  if (isAuthorised) {
+    updatedTweet = await Tweet.findByIdAndUpdate(req.user?._id, {
+      $set: {
+        content
+      }
+    })
+  }
   return res.status(200).json(new ApiResponse(200, updatedTweet, "Tweet updated"))
   
 })
