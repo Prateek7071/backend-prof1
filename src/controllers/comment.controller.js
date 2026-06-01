70617230b03a38426bfea7bcfbf700e39087762d
Prateek7071 <tiwariprateek123@gmail.com>
2026-06-01 18:30:38 +0530

    created add comment and update comment

---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,74 @@
+import mongoose from "mongoose";
+import { Comment } from "../models/comment.models.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+
+const getVideoComments = asyncHandler(async (req, res) => {
+  //TODO: get all comments for a video
+  const {videoId} = req.params
+  const {page = 1, limit = 10} = req.query
+  if(!videoId) {
+    throw new ApiError(400, "Video not found")
+  }
+})
+
+const addComment = asyncHandler(async (req, res) => {
+  const { videoId } = req.params
+  const { content } = req.body
+  
+  if(!videoId) {
+    throw new ApiError(400, "Video not found")
+  }
+
+  if(!content) {
+    throw new ApiError(400, "Comment field cant be empty")
+  }
+  const comment = await Comment.create({
+    content,
+    video: videoId,
+    owner: req.user?._id
+  })
+
+  if (!comment) {
+    throw new ApiError(500,"Comment failed to publish")
+  }
+
+  return res.status(200).json(new ApiResponse(200,comment,"Comment published"))
+})
+
+const updateComment = asyncHandler(async (req, res) => {
+  const { commentId } = req.params
+  const { content } = req.body
+
+  if (!commentId) {
+    throw new ApiError(400,"Comment doesnt exist")
+  }
+  if (!content) {
+    throw new ApiError(400,"Nothing to update")
+  }
+  const isAuthorised = await Comment.findOne({
+    owner: req.user?._id,
+    _id: commentId
+  })
+  if (!isAuthorised) {
+    throw new ApiError(403,"Not authorised to update comment")
+  }
+  let updatedComment=""
+  if (isAuthorised) {
+    updatedComment = await Comment.findByIdAndUpdate(
+      commentId , {
+        $set: { content }
+    }) 
+  }
+
+  if (!updatedComment) {
+    throw new ApiError(500,"cant update comment")
+  }
+
+  return res.status(200).json(new ApiResponse(200,updatedComment,"Comment updated"))
+})
+export {
+    addComment,updateComment
+}
