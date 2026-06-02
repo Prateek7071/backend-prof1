33752bff7d60144d1e9ad0bd1473966eda2ae917
Prateek7071 <tiwariprateek123@gmail.com>
2026-06-02 17:28:29 +0530

    added get all video route

--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { addComment, deleteComment, updateComment } from "../controllers/comment.controller.js";
+import { addComment, deleteComment, getVideoComments, updateComment } from "../controllers/comment.controller.js";
 
 const router = Router()
 
 router.use(verifyJWT)
 
-router.route("/:videoId").post(addComment)
+router.route("/:videoId").post(addComment).get(getVideoComments)
 router.route("/c/:commentId").patch(updateComment).delete(deleteComment)
 
 export default router
