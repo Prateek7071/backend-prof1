3e41071ea0b699c37be8c2e71abd6189e528a212
Prateek7071 <tiwariprateek123@gmail.com>
2026-06-02 22:15:25 +0530

    changed being all name unique to name of playlist per user unique

--- src/models/playlist.models.js
+++ src/models/playlist.models.js
@@ -4,23 +4,24 @@ const playlistSchema =new Schema({
   name: {
     type: String,
     required: true,
-    unique: true,
+    // unique: true,
     trim: true,
   },
   description: {
     type: String,
     required: true,
-    unique: true,
     trim: true,
   },
-  videos: {
+  videos: [{
     type: Schema.Types.ObjectId,
     ref:"Video"
-  },
+  }],
   owner: {
     type: Schema.Types.ObjectId,
     ref:"User"
   }
 }, { timestamps: true })
+
+playlistSchema.index({ name: 1, owner: 1 }, { unique: true });
 
 export const Playlist = mongoose.model("Playlist",playlistSchema)
