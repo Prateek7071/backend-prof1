30e47978adf1b31e206796c08eb2a5b04c37e3bf
Prateek7071 <tiwariprateek123@gmail.com>
2026-06-02 21:03:31 +0530

    minor fix

--- src/routes/tweet.route.js
+++ src/routes/tweet.route.js
@@ -3,14 +3,11 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { createTweet, deleteTweet, getUserTweets, updateTweet } from "../controllers/tweet.controller.js";
 
 const router = Router()
+router.use(verifyJWT)
 
-router.route("/create-tweet").post(
-  verifyJWT,
-  createTweet
-)
+router.route("/").post(createTweet)
 
-router.route("/all-tweets").get(verifyJWT, getUserTweets)
-router.route("/update-tweet").post(verifyJWT, updateTweet)
-router.route("/delete-tweet/:tweetId").delete(verifyJWT, deleteTweet)
+router.route("/user/:userId").get(getUserTweets)
+router.route("/:tweetId").patch(updateTweet).delete(deleteTweet)
 
 export default router
